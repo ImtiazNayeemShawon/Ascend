0a9f7caa440196c888bbea1a770b79a370004989
imtiaz <imtiaznayim@gmail.com>
2023-02-03 15:51:18 +0600

    initializ

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "accent-1": "#333",
+      },
+    },
+  },
+  variants: {},
+  plugins: [require("flowbite/plugin")],
+  theme: {},
+};
