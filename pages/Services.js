e99ef6e77c1a5c99ac79d359d7aa3903a98ac930
imtiaz <imtiaznayim@gmail.com>
2023-02-05 16:40:00 +0600

    build command fixed

--- pages/Services.js
+++ pages/Services.js
@@ -11,7 +11,7 @@ export default function Services() {
         <h1 className="text-center text-4xl header mt-40">Our services</h1>
         <div className="grid grid-cols-4 text-center place-items-center max-sm:grid-cols-1 mt-20">
           <div className="bg-black h-80 w-80 rounded-lg p-4  border-2  border-lime-200">
-            <Image className="m-auto" src={Web} width={100} height={150} />
+            <Image className="m-auto" src={Web} width={100} height={150} alt="website"/>
             <h1 className="header mt-12">Web development</h1>
             <p className="text-center text-sm text-gray-400 mt-5">
               We make website for promote you buisness using modern technology
